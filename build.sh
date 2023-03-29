da40611f5dd99a28c140bceed596376708b93876
某亚瑟 <mouyase@qq.com>
2023-03-29 10:53:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-version="20.1-Nexus"
+releases_url=https://api.github.com/repos/open-tdp/tdp-cloud/releases/latest
+version=`curl $releases_url | grep tag_name | cut -f4 -d "\""`
 source_img_name="CoreELEC-Amlogic-ng.arm-${version}-Generic"
 source_img_file="${source_img_name}.img.gz"
 source_img_url="https://github.com/CoreELEC/CoreELEC/releases/download/${version}/${source_img_name}.img.gz"
